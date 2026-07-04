28cdd6430e6a67a6bcd5db277c740dfe729a2d33
mina <minaaa5814@gmail.com>
2026-07-04 16:07:51 +0900

    feat: add graceful server shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/minaamim/go-chat-server/internal/chat"
 	"github.com/minaamim/go-chat-server/internal/http/server"
@@ -13,8 +18,28 @@ func main() {
 	go hub.Run()
 
 	router := server.NewRouter(hub)
-	log.Println("server started :8080")
-	if err := http.ListenAndServe(":8080", router); err != nil {
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		log.Println("server started :8080")
+
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatal(err)
 	}
 }
